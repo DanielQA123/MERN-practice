034094375929c6adc214980c20d613e1e5e943cb
DanielQA123 <DAhuchogu@QA.com>
2021-02-19 20:52:17 +0000

    DA: Method for requests

---
+++ routes/products.js
@@ -0,0 +1,18 @@
+'use strict;'
+
+const router = require('express').Router();
+
+//request methods => CRUD
+//GET:
+router.get("/getAll", (req, res) =>{
+    res.send(`Here is the information needed .....`);
+});
+
+//POST: 
+router.get("/create", (req, res) =>{
+    const name = req.body.name;
+    res.send(`Hello there ${name}`);
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,18 +5,9 @@ const express = require('express');
 //Initalize the app
 const app = express();
 
-//Requests
-app.get("/", (req, res) => {
-    res.send("hello world!");
-});
+const productRoute = require("./routes/products");
 
-app.get("/hello", (req, res) => {
-    res.send("This is the hello world route");
-});
-
-app.get("/error",(res,rep) =>{
-    res.status(500).send("oops");
-});
+app.use("/product", productRoute);
 
 //communicate with the app on a specific port
 const server = app.listen(6020, () =>{
