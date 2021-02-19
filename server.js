065fc22b3d8e37e8845a969c87128567a06c3305
DanielQA123 <DAhuchogu@QA.com>
2021-02-19 19:21:07 +0000

    DA: Changed my port number

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const express = require('express');
 //Initalize the app
 const app = express();
 
-//communicate with the app on a specific port
-
-app.listen(5019);
-
 //Requests
 app.get("/", (req, res) => {
     res.send("hello world!");
@@ -16,4 +12,13 @@ app.get("/", (req, res) => {
 
 app.get("/hello", (req, res) => {
     res.send("This is the hello world route");
+});
+
+app.get("/error",(res,rep) =>{
+    res.status(500).send("oops");
+});
+
+//communicate with the app on a specific port
+const server = app.listen(6020, () =>{
+    console.log(`Server has successfully started on port number: ${server.address().port}`);
 });
