31f17c2a035c127e9073997d39acf80ca5ee8eb9
DanielQA123 <DAhuchogu@QA.com>
2021-02-24 16:08:11 +0000

    DA: Routing work from Sav's lecture

---
+++ frontend/src/Components/09-Routing/Product.jsx
@@ -0,0 +1,12 @@
+'use strict'; 
+
+const Product = () => {
+    return(
+        <div>
+            <p>Hairbrush</p>
+            <p>Candle</p>
+            <p>Laptop</p>
+        </div>
+    )
+}
+export default Product; 
---
+++ frontend/src/Components/09-Routing/User.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import {useParams} from 'react-router-dom';
+
+const User = () => {
+
+    const {id} = useParams(); 
+
+    return( <p>The value from the URL is: {id}</p> )
+
+}
+
+export default User; 
