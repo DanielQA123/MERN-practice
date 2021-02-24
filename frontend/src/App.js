48c831d8ad229d42dfc8bda726c630c7686d2f75
DanielQA123 <DAhuchogu@QA.com>
2021-02-24 17:11:25 +0000

    DA: Adding Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
 'use strict';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./Components/09-Routing/Home";
+import About from "./Components/09-Routing/About";
+import Product from "./Components/09-Routing/Product";
+import Nav from "./Components/09-Routing/Nav";
+
+
 import Introduction from "./Components/01-Intro/First";
 import Second from "./Components/01-Intro/Second";
 import Biopic from "./Components/01-Intro/Biopic";
@@ -13,17 +20,29 @@ import EmployeeTutorial from "./Components/10-Data-Requests/EmployeeTutorial";
 
 const App = () => {
   return (
-    <div className="App">
-      <p>Dan-Dan says good morning</p>
-      {/* <Introduction/>
-      <Second/> */}
-      {/* <Biopic/>
-      <Standard/> */}
-      {/* <PropsComp/> */}
-      {/* <People/> */}
-      <EmployeeTutorial/>
+    <div className="container">
+      <Router>
+        <Nav/>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path ="/Product">
+            <Product/>
+          </Route>
+          <Route path="/EmployeeTutorial">
+          <EmployeeTutorial/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
