b7c2d9aa73b773783ae0242dd164fd83d253de50
DanielQA123 <DAhuchogu@QA.com>
2021-02-25 16:08:09 +0000

    Made changes

--- frontend/src/Components/10-Data-Requests/OMDb-Task/Search.jsx
+++ frontend/src/Components/10-Data-Requests/OMDb-Task/Search.jsx
@@ -1,3 +1,8 @@
+import axios from 'axios';
+import { API_URL } from './CONSTS.json';
+import { useHistory } from 'react-router-dom';
+import SearchBar from './SearchBar';
+
 
 
 const Search = () => {
@@ -9,10 +14,43 @@ const Search = () => {
     const [err, setErr] = useState('');
     const [films, setFilms] = useState([]);
 
-    const handleChange = ({target}) => {
-        
+    const handleChange = ({ target }) => {
+        setQuery(target.value);
     }
-    
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        axios.get(`${API_URL}&s=${query}`)
+            .then(res => {
+                if (res.data.Error) {
+                    setErr(res.data.Error);
+                } else {
+                    setFilms(res.data.Search);
+                    setErr('');
+                }
+            })
+            .catch(err => {
+                setErr(err);
+            })
+    }
+
+    const getDetails = (id) => {
+        console.log(id);
+        history.push(`/OMDB/details/${id}`);
+    }
+
+    return (
+        <>
+            <SearchBar query={query} handleChange={handleChange} search={handleSearch} />
+            {err ?
+                <div className="alert alert-danger">{err}</div>
+                :
+                <SearchResults films={films} getDetails={getDetails} />
+            }
+        </>
+    )
 
 
 }
+
+export default Search;
